f899a75f3a3085948ad3897dc2347f2c0260826e
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-25 16:58:04 +0300

    feat(view): add ProductListView - add ProductListView - include Product urls to common urls - add products/urls.py - add product_list.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = 'products/product_list.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        return context
+
 
 # Create your views here.
