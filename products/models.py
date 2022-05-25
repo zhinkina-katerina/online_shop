5279b7a89070317a1d749ce4b2ebb289e60bfd01
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-25 22:50:20 +0300

    feat(model): add ProductManager - add ProductManager - add ProductQuerySet - add featured Field in Product - add migration with featured Field

--- products/models.py
+++ products/models.py
@@ -21,12 +21,32 @@ def upload_image_path(instance, filename):
                                                     new_filename=new_filename
                                                 )
 
+class ProductQuerySet(models.query.QuerySet):
+    def featured(self):
+        return self.filter(featured=True)
+
+
+class ProductManager (models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def features(self):
+        return self.get_queryset().featured()
+
+    def get_by_id(self, id):
+        queryset = self.get_queryset().filter(id=id)
+        if queryset.count() == 1:
+            return queryset.first()
+        return None
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True )
+    featured = models.BooleanField(default=False)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
