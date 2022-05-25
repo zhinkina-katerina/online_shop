25a6b7d5ca5a26b6c60e336eaf0ea13ce3a5d730
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-25 16:21:32 +0300

    fix(model):edit model Product - add model to admin.py - add functions: __str__, __unicode__

--- products/models.py
+++ products/models.py
@@ -4,3 +4,9 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
