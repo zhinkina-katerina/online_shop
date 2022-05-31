83b741804fb138b492ea5b77168bb139a084845b
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-05-31 11:32:31 +0300

    feat(model): add Cart model - register Cart model in admin

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(default=0.00, decimal_places=2, max_digits=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
